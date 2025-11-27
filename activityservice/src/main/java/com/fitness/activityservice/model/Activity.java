b439b403ca967b5c0b8c487dca3b6e9ce143c897
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-11-27 15:26:21 +0530

    git init

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -0,0 +1,16 @@
+package com.fitness.activityservice.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class Activity {
+    private String id;
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additionalMetrics;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
