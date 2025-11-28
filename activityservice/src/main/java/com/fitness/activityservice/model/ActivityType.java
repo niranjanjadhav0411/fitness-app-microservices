b06cc679c62500db0487f6b290fac7746ef6892f
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-11-28 17:25:08 +0530

    git init

--- activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -1,14 +1,14 @@
 package com.fitness.activityservice.model;
 
-public class ActivityType {
-//    RUNNING,
-//    WALKING,
-//    CYCLING,
-//    SWIMMING,
-//    WEIGHT_TRAINING,
-//    YOGA,
-//    HIIT,
-//    CARDIO,
-//    STRETCHING,
-//    OTHER
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
 }
---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,12 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityService {
+    public @Nullable ActivityResponse trackActivity(ActivityRequest request) {
+    }
+}
