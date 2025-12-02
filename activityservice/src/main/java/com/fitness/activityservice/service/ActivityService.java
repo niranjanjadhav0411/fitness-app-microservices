fc713115a7703e55b9b95e27f774e463c7b25580
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-12-02 19:39:58 +0530

    git init

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ActivityService {
@@ -40,5 +43,12 @@ public class ActivityService {
         response.setCreatedAt(activity.getCreatedAt());
         response.setUpdatedAt(activity.getUpdatedAt());
         return response;
+    }
+
+    public @Nullable List<ActivityResponse> getUserActivities(String userId) {
+        List<Activity> activities = activityRepository.findByUserId(userId);
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
     }
 }
