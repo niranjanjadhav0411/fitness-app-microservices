9a0b0e6f513d6f85c86be2bd8e5bae5300ebd2d3
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-12-11 22:14:22 +0530

    git init

--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -1,15 +1,15 @@
 package com.fitness.aiservice.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.loadbalancer.reactive.WebClientCustomizer;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
 @Service
 public class GeminiService {
 
-    private final WebClientCustomizer webClient;
+    private final WebClient webClient;
 
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
@@ -17,7 +17,7 @@ public class GeminiService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
-    public GeminiService(WebClientCustomizer.Builder webClientBuilder) {
+    public GeminiService(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.build();
     }
 
