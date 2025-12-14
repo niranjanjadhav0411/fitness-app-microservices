10cc59c21015d4c764f41d9e20d7e4afe8fa1165
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-12-14 21:40:41 +0530

    git init

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import sun.security.krb5.internal.ccache.FileCredentialsCache;
 
 @Service
 @Slf4j
@@ -15,6 +16,7 @@ public class ActivityMessageListener {
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
         Recommendation recommendation = aiService.generateRecommendation(activity);
+        FileCredentialsCache recommendationRepository;
         recommendationRepository.save(recommendation);
     }
 }
