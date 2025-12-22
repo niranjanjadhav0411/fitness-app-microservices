1e0f90dc808067743c24071f793f3490ae4cfe9f
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-12-22 21:54:02 +0530

    git init

--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -1,11 +1,18 @@
 package com.fitness.aiservice.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class GeminiService {
     private final RestClient restClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
 
     public GeminiService(RestClient.Builder restClientBuilder) {
         this.restClient = restClientBuilder.build();
