ff0e35bc79096184de5fac720d62ead1479bcf06
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-12-25 21:59:14 +0530

    git init

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityAIService.java
@@ -0,0 +1,21 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityAIService {
+    private final GeminiService geminiService;
+
+    public Recommendation generateRecommendation(Activity activity) {
+        String prompt = createPromptForActivity(activity);
+        String aiResponse = geminiService.getAnswer(prompt);
+        log.info("RESPONSE FROM AI: {} ", aiResponse);
+        return processAiResponse(activity, aiResponse);
+    }
+}
